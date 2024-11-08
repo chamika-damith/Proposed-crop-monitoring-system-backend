bf41c70ed13135a24464949ef26db40603dbde53
chamika-damith <chamikadamith9@gmail.com>
2024-11-08 09:06:12 +0530

    Initial commit

---
+++ src/main/java/com/example/proposedcropmonitoringsystembackend/ProposedCropmonitoringsystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.proposedcropmonitoringsystembackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProposedCropmonitoringsystemBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProposedCropmonitoringsystemBackendApplication.class, args);
+    }
+
+}
