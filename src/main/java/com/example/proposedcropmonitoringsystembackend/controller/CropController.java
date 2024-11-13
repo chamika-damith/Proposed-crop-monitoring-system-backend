419a033ce5cf341b76d3835800adbbc21733c67b
chamika-damith <chamikadamith9@gmail.com>
2024-11-13 23:03:28 +0530

    update crop details method done

--- src/main/java/com/example/proposedcropmonitoringsystembackend/controller/CropController.java
+++ src/main/java/com/example/proposedcropmonitoringsystembackend/controller/CropController.java
@@ -10,10 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -59,6 +56,43 @@ public class CropController {
         }
 
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+
+    @PutMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> updateCrop(
+            @RequestParam("cropCode") String cropCode,
+            @RequestParam("commonName") String commonName,
+            @RequestParam("scientificName") String scientificName,
+            @RequestParam("category") String category,
+            @RequestParam("season") String season,
+            @RequestParam("image") MultipartFile image,
+            @RequestParam("fieldCode") String fieldCode){
+
+
+        String base64ProPic = "";
+
+        try {
+            byte[] bytesProPic = image.getBytes();
+            base64ProPic = AppUtil.profilePicToBase64(bytesProPic);
+
+            FieldDTO fieldDTO = fieldService.get(fieldCode);
+            if (fieldDTO == null) {
+                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+            }
+
+
+            CropDTO cropDTO = new CropDTO(cropCode, commonName, scientificName, base64ProPic, category, season, fieldDTO);
+
+            cropService.update(cropCode,cropDTO);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 
 
--- src/main/java/com/example/proposedcropmonitoringsystembackend/service/impl/CropServiceImpl.java
+++ src/main/java/com/example/proposedcropmonitoringsystembackend/service/impl/CropServiceImpl.java
@@ -6,6 +6,7 @@ import com.example.proposedcropmonitoringsystembackend.entity.impl.CropEntity;
 import com.example.proposedcropmonitoringsystembackend.entity.impl.FieldEntity;
 import com.example.proposedcropmonitoringsystembackend.service.CropService;
 import com.example.proposedcropmonitoringsystembackend.util.Mapping;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,14 +46,27 @@ public class CropServiceImpl implements CropService {
     @Override
     public void update(String id, CropDTO dto) {
         Optional<CropEntity> byId = cropDao.findById(id);
+
         if (byId.isPresent()) {
-            byId.get().setCategory(dto.getCategory());
-            byId.get().setImage(dto.getImage());
-            byId.get().setSeason(dto.getSeason());
-            byId.get().setCommonName(dto.getCommonName());
-            byId.get().setScientificName(dto.getScientificName());
+            CropEntity cropEntity = byId.get();
+
+            cropEntity.setCategory(dto.getCategory());
+            cropEntity.setImage(dto.getImage());
+            cropEntity.setSeason(dto.getSeason());
+            cropEntity.setCommonName(dto.getCommonName());
+            cropEntity.setScientificName(dto.getScientificName());
+
+            if (dto.getFieldDTO() != null) {
+                FieldEntity fieldEntity = mapping.toFieldEntity(dto.getFieldDTO());
+                cropEntity.setFieldEntity(fieldEntity);
+            }
+
+            cropDao.save(cropEntity);
+        } else {
+            throw new EntityNotFoundException("Crop entity with ID " + id + " not found.");
         }
     }
+
 
     @Override
     public CropDTO get(String id) {
