e8a6cd398c3c95ab5874c30dda3596efc7ffe33c
chamika-damith <chamikadamith9@gmail.com>
2024-11-14 13:23:50 +0530

    update staff done

--- src/main/java/com/example/proposedcropmonitoringsystembackend/controller/StaffController.java
+++ src/main/java/com/example/proposedcropmonitoringsystembackend/controller/StaffController.java
@@ -4,22 +4,28 @@ import com.example.proposedcropmonitoringsystembackend.dto.impl.StaffDTO;
 import com.example.proposedcropmonitoringsystembackend.service.StaffService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/staff")
 public class StaffController {
     @Autowired
     private StaffService staffService;
-    @PostMapping
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> saveStaff(@RequestBody StaffDTO staffDTO){
 
         staffService.save(staffDTO);
 
         return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/{staffId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateStaff(@RequestBody StaffDTO staffDTO,@PathVariable("staffId") String staffId){
+
+        staffService.update(staffId,staffDTO);
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
--- src/main/java/com/example/proposedcropmonitoringsystembackend/service/impl/StaffServiceImpl.java
+++ src/main/java/com/example/proposedcropmonitoringsystembackend/service/impl/StaffServiceImpl.java
@@ -6,11 +6,14 @@ import com.example.proposedcropmonitoringsystembackend.entity.impl.FieldEntity;
 import com.example.proposedcropmonitoringsystembackend.entity.impl.StaffEntity;
 import com.example.proposedcropmonitoringsystembackend.service.StaffService;
 import com.example.proposedcropmonitoringsystembackend.util.Mapping;
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 @Transactional
 public class StaffServiceImpl implements StaffService {
@@ -36,7 +39,31 @@ public class StaffServiceImpl implements StaffService {
 
     @Override
     public void update(String id, StaffDTO dto) {
+        Optional<StaffEntity> byId = staffDao.findById(id);
 
+        if (byId.isPresent()) {
+            StaffEntity staffEntity = byId.get();
+
+            staffEntity.setFirstName(dto.getFirstName());
+            staffEntity.setLastName(dto.getLastName());
+            staffEntity.setDesignation(dto.getDesignation());
+            staffEntity.setGender(dto.getGender());
+            staffEntity.setJoinedDate(dto.getJoinedDate());
+            staffEntity.setDob(dto.getDob());
+            staffEntity.setAddress(dto.getAddress());
+            staffEntity.setContact(dto.getContact());
+            staffEntity.setEmail(dto.getEmail());
+            staffEntity.setRole(dto.getRole());
+
+            if (dto.getFields() != null) {
+                List<FieldEntity> fieldEntityList = mapping.asFieldEntityList(dto.getFields());
+                staffEntity.setFields(fieldEntityList);
+            }
+
+            staffDao.save(staffEntity);
+        } else {
+            throw new EntityNotFoundException("Staff entity with ID " + id + " not found.");
+        }
     }
 
     @Override
